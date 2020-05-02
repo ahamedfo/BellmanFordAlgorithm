b2a260ef3576da3e0591b00d8cceb986935b0b41
ahamedfofana <ahamedfofana12@gmail.com>
2020-05-02 16:21:50 -0400

    Bellman Ford edges

--- Solution.py
+++ Solution.py
@@ -8,7 +8,18 @@ class Solution:
         ############### YOUR CODE GOES HERE ##################
         distance = {}
         predecessor = {}
-        for vertex  in self.incoming_edges:
-            
+        print(self.outgoing_edges)
+        for vertex in range(0, self.outgoing_edges):
+            distance[vertex] = float("inf")
+            predecessor[vertex] = None
+
+        distance[self.origin] = 0
+        print(self.outgoing_edges)
+        for i in range(1, len(self.outgoing_edges_edges) - 1):
+            curNode = self.outgoing_edges[i]
+            for edges in curNode:
+                if distance[i] + curNode[edges] < distance[edges]:
+                    distance[edges] := distance[i] + curNode[edges]
+                # if distance[i] +
 
         return [] #Return empty
